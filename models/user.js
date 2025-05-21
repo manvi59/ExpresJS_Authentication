478f3d1beab5f6619aa0d66be3947ff4c3304c72
manvi59 <manviyogi59@gmail.com>
2025-05-21 15:15:03 +0530

    monoose connection update

--- models/user.js
+++ models/user.js
@@ -1,10 +1,11 @@
 const mongoose=require("mongoose");
 require("dotenv").config();
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+// mongoose.connect(process.env.MONGO_URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// })
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("✅ Connected to MongoDB"))
 .catch(err => console.error("❌ MongoDB connection error:", err));
 
