5926ffdb976d7337da63533b82347d040740f08d
manvi59 <manviyogi59@gmail.com>
2025-05-21 13:14:40 +0530

    updation

--- models/user.js
+++ models/user.js
@@ -1,8 +1,6 @@
 const mongoose=require("mongoose");
 require("dotenv").config();
 
-// mongoose.connect(`mongodb://127.0.0.1:27017/authtestapp`);
-
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
